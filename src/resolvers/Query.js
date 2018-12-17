c901b790bc663a61f87f9bdc185252138ce59986
ricardo <masmerino@gmail.com>
2018-12-17 10:40:54 -0600

    User profile query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,17 @@
 const feed = (root, args, context, info) => context.db.query.links({}, info);
 const users = (root, args, context, info) => context.db.query.users({}, info);
-const me = (root, args, context, info) => 
+const me = (parent, args, ctx, info) => {
+  // check if there is a current user ID
+  if (!ctx.request.userId) {
+    return null;
+  }
+  return ctx.db.query.user(
+    {
+      where: { id: ctx.request.userId },
+    },
+    info
+  );
+}
 
 module.exports = {
   feed,
